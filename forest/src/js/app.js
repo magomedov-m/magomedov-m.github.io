01082c450359608f8e588cafc106dfce13a8812a
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2024-08-01 21:13:08 +0300

    Update app.js

--- forest/src/js/app.js
+++ forest/src/js/app.js
@@ -3,14 +3,12 @@
 const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxPoints > 0;
 
 function handleMouseMove(e) {
-    document.addEventListener('mousemove', e => {
-        Object.assign(document.documentElement, {
-            style: `
-                --move-x: ${(e.clientX - window.innerWidth / 2) * -0.008}deg;
-                --move-y: ${(e.clientY - window.innerHeight / 2) * -0.02}deg;
-            `
-        })
-    })
+    //document.addEventListener('mousemove', e => {
+        Object.assign(document.documentElement.style {
+                '--move-x': `${(e.clientX - window.innerWidth / 2) * -0.008}deg`;
+                '--move-y': `${(e.clientY - window.innerHeight / 2) * -0.02}deg`;
+        });
+    //})
 }
 
 function handleDeviceOrientation(e) {
