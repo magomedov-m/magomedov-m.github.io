3fc9bc09df4e85c783d5ffcdab42f39e6a9e5249
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2024-08-01 21:09:27 +0300

    Update app.js

--- forest/src/js/app.js
+++ forest/src/js/app.js
@@ -1,22 +1,31 @@
 'use strict'
 
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-            --move-x: ${(e.clientX - window.innerWidth / 2) * -0.008}deg;
-            --move-y: ${(e.clientY - window.innerHeight / 2) * -0.02}deg;
-        `
-    })
-})
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxPoints > 0;
 
-window.addEventListener('deviceorientation', function(e) => {
+function handleMouseMove(e) {
+    document.addEventListener('mousemove', e => {
+        Object.assign(document.documentElement, {
+            style: `
+                --move-x: ${(e.clientX - window.innerWidth / 2) * -0.008}deg;
+                --move-y: ${(e.clientY - window.innerHeight / 2) * -0.02}deg;
+            `
+        })
+    })
+}
+
+function handleDeviceOrientation(e) {
     const x = e.beta;
     const y = e.gamma;
 
-    Object.assign(document.documentElement, {
-        style: `
-            --move-x: ${(x - window.innerWidth / 2) * -0.008}deg;
-            --move-y: ${(y - window.innerHeight / 2) * -0.02}deg;
-        `
-    })
-})
+    Object.assign(document.documentElement.style, {
+        
+            '--move-x': `${(y) * 0.5}deg`;
+            '--move-y': `${(x) * 0.5}deg`;
+    });
+}
+
+if (isTouchDevice) {
+    window.addEventListener('deviceorientation', handleDeviceOrientation);
+}else {
+    document.addEventListener('mousemove', handleMouseMove);
+}
